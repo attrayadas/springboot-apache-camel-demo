46d427f2845be6d7b03188821a843fdb43899ede
Attraya Das <attrayaghoshdas@gmail.com>
2023-11-28 23:54:05 +0530

    Initial Commit

---
+++ src/main/java/com/attraya/SpringbootApacheCamelDemoApplication.java
@@ -0,0 +1,14 @@
+package com.attraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheCamelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApacheCamelDemoApplication.class, args);
+
+	}
+
+}
